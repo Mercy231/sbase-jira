9e782d768481d033eb729f382cea19068d470ee2
Mercy <yt.tishchenko.07@gmail.com>
2023-04-13 10:04:54 +0300

    SWE-11 (updated v2)

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,7 +1,5 @@
-let comment = true
-
 $(".comment-create-btn").click(createComment)
-$(".comment-edit-btn").click(updateComment)
+$(".comment-edit-btn").click(showUpdateComment)
 $(".comment-delete-btn").click(destroyComment)
 
 function createComment () {
@@ -20,13 +18,25 @@ function createComment () {
                 $(`#${id} .comment-create-msg`).text('Created successfully')
                 $(`#${id} .comment-text`).val('')
                 $(`#${id} #comments`).prepend(response.html)
-                $(`#${id} #comments:last-child`).find('.comment-edit-btn').bind('click', updateComment)
-                $(`#${id} #comments:last-child`).find('.comment-delete-btn').bind('click', destroyComment)
+                $(`#${id} #comments .comment-edit-btn`).first().bind('click', showUpdateComment)
+                $(`#${id} #comments .comment-delete-btn`).first().bind('click', destroyComment)
             } else {
                 $(`#${id} .comment-create-msg`).text(response.error)
             }
         }
     })
+}
+function showUpdateComment () {
+    let id = $(this).parent().parent().parent().parent().parent().attr('id')
+    let commId = $(this).parent().parent().attr('id')
+    let text = $(`#${id} #${commId} .text`)
+    let btn = $(`#${id} #${commId} .comment-edit-btn`)
+    btn.text('Save').toggle()
+    $(".comment-edit-btn").toggle()
+    $(".comment-delete-btn").toggle()
+    text.replaceWith(`<textarea class="text">${text.text()}</textarea>`)
+    btn.unbind('click')
+    btn.bind('click', updateComment)
 }
 function updateComment () {
     let id = $(this).parent().parent().parent().parent().parent().attr('id')
@@ -34,54 +44,45 @@ function updateComment () {
     let text = $(`#${id} #${commId} .text`)
     let msg = $(`#${id} #${commId} .comment-msg`)
     let btn = $(`#${id} #${commId} .comment-edit-btn`)
-    switch(comment) {
-    case true:
-        btn.text('Save').toggle()
-        $(".comment-edit-btn").toggle()
-        $(".comment-delete-btn").toggle()
-        text.replaceWith(`<textarea class="text">${text.text()}</textarea>`)
-        comment = !comment
-        break
-    case false:
-        $.ajax({
-            url: `/comment/${commId}`,
-            type: 'PATCH',
-            dataType: 'json',
-            data: {
-                "_token": $('meta[name="csrf-token"]').attr('content'),
-                text: text.val(),
-            },
-            success: function(response){
-                if (response.success) {
-                    text.replaceWith(`<p class="text">${text.val()}</p>`)
-                    msg.text('')
-                    btn.text('Edit').toggle()
-                    $(".comment-edit-btn").toggle()
-                    $(".comment-delete-btn").toggle()
-                    comment = !comment
-                } else {
-                    msg.text(response.error)
-
-                }
+    $.ajax({
+        url: `/comment/${commId}`,
+        type: 'PATCH',
+        dataType: 'json',
+        data: {
+            "_token": $('meta[name="csrf-token"]').attr('content'),
+            text: text.val(),
+        },
+        success: function(response){
+            if (response.success) {
+                text.replaceWith(`<p class="text">${text.val()}</p>`)
+                msg.text('')
+                btn.text('Edit').toggle()
+                $(".comment-edit-btn").toggle()
+                $(".comment-delete-btn").toggle()
+                btn.unbind('click')
+                btn.bind('click', showUpdateComment)
+            } else {
+                msg.text(response.error)
             }
-        });
-        break
-    }
+        }
+    });
 }
 function destroyComment () {
     let id = $(this).parent().parent().parent().parent().parent().attr('id')
     let commId = $(this).parent().parent().attr('id')
-    $.ajax({
-        url: `/comment/${commId}`,
-        type: 'DELETE',
-        dataType: 'json',
-        data: {
-            "_token": $('meta[name="csrf-token"]').attr('content'),
-        },
-        success: function(response){
-            if (response.success) {
-                $(`#${id} #${commId}`).remove()
+    if (confirm("Delete comment?")) {
+        $.ajax({
+            url: `/comment/${commId}`,
+            type: 'DELETE',
+            dataType: 'json',
+            data: {
+                "_token": $('meta[name="csrf-token"]').attr('content'),
+            },
+            success: function (response) {
+                if (response.success) {
+                    $(`#${id} #${commId}`).remove()
+                }
             }
-        }
-    });
+        });
+    }
 }
--- public/js/post.js
+++ public/js/post.js
@@ -1,7 +1,5 @@
-let bool = true
-
 $(".post-create-btn").click(createPost)
-$(".post-edit-btn").click(updatePost)
+$(".post-edit-btn").click(showUpdatePost)
 $(".post-delete-btn").click(destroyPost)
 
 function createPost () {
@@ -19,7 +17,7 @@ function createPost () {
                 $("#post-create-msg").text('Created successfully')
                 $("#post-create-title").val('')
                 $("#post-create-text").val('')
-                $("#posts").prepend(response.html).find('.post-edit-btn').bind('click', updatePost)
+                $("#posts").prepend(response.html).find('.post-edit-btn').bind('click', showUpdatePost)
                 $("#posts").find('.post-delete-btn').bind('click', destroyPost)
             } else {
                 $("#post-create-msg").text(response.error)
@@ -27,47 +25,49 @@ function createPost () {
         }
     });
 }
+function showUpdatePost () {
+    let id = $(this).parent().parent().attr('id')
+    let title = $(`#${id} .title:first`)
+    let text = $(`#${id} .text:first`)
+    let btn = $(`#${id} .post-edit-btn`)
+    btn.text('Save').toggle()
+    $(".post-edit-btn").toggle()
+    $(".post-delete-btn").toggle()
+    title.replaceWith(`<input type='text' class="title" value="${title.text()}">`)
+    text.replaceWith(`<textarea class="text">${text.text()}</textarea>`)
+    btn.unbind('click')
+    btn.bind('click', updatePost)
+}
 function updatePost () {
     let id = $(this).parent().parent().attr('id')
     let title = $(`#${id} .title`)
     let text = $(`#${id} .text`)
     let msg = $(`#${id} .post-msg`)
     let btn = $(`#${id} .post-edit-btn`)
-    switch (bool) {
-        case true:
-            btn.text('Save').toggle()
-            $(".post-edit-btn").toggle()
-            $(".post-delete-btn").toggle()
-            title.replaceWith(`<input type='text' class="title" value="${title.text()}">`)
-            text.replaceWith(`<textarea class="text">${text.text()}</textarea>`)
-            bool = !bool
-            break
-        case false:
-            $.ajax({
-                url: `/post/${id}`,
-                type: 'PATCH',
-                dataType: 'json',
-                data: {
-                    "_token": $('meta[name="csrf-token"]').attr('content'),
-                    title: title.val(),
-                    text: text.val(),
-                },
-                success: function(response){
-                    if (response.success) {
-                        title.replaceWith(`<h2 class="title">${title.val()}</h2>`)
-                        text.replaceWith(`<p class="text">${text.val()}</p>`)
-                        msg.text('')
-                        btn.text('Edit').toggle()
-                        $(".post-edit-btn").toggle()
-                        $(".post-delete-btn").toggle()
-                        bool = !bool
-                    } else {
-                        msg.text(response.error)
-                    }
-                }
-            });
-            break
-    }
+    $.ajax({
+        url: `/post/${id}`,
+        type: 'PATCH',
+        dataType: 'json',
+        data: {
+            "_token": $('meta[name="csrf-token"]').attr('content'),
+            title: title.val(),
+            text: text.val(),
+        },
+        success: function(response){
+            if (response.success) {
+                title.replaceWith(`<h2 class="title">${title.val()}</h2>`)
+                text.replaceWith(`<p class="text">${text.val()}</p>`)
+                msg.text('')
+                btn.text('Edit').toggle()
+                $(".post-edit-btn").toggle()
+                $(".post-delete-btn").toggle()
+                btn.unbind('click')
+                btn.bind('click', showUpdatePost)
+            } else {
+                msg.text(response.error)
+            }
+        }
+    });
 }
 function destroyPost () {
     let id = $(this).parent().parent().attr('id')
